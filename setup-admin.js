0def069c08ba652551da26c953fca2c6a606d780
xiechao06 <xiechoa06@gmail.com>
2016-08-13 21:31:33 +0800

    fix #6

--- setup-admin.js
+++ setup-admin.js
@@ -3,6 +3,7 @@ var config = require('./config.js');
 var pgp = require('pg-promise')();
 var logger = require('./logger.js');
 var db = pgp(config.get('dbConnection'));
+var roles = require('./roles');
 
 var admin = config.get('admin');
 
@@ -11,7 +12,7 @@ db.query(
     INSERT INTO users (username, password, role) VALUES
     ($1, crypt($2, gen_salt('md5')), $3);
     `, 
-    [admin.username, admin.password, 'admin']
+    [admin.username, admin.password, roles.ADMIN]
 ).then(function () {
   logger.info('completed');
   pgp.end();
