fd09c8290db372bc3956a0c4b8380ea7e6575bd3
xiechao06 <xiechao06@gmail.com>
2016-08-18 18:22:44 +0800

    0

--- initialize.js
+++ initialize.js
@@ -8,11 +8,8 @@ var cofy = require('cofy');
 
 var admin = config.get('admin');
 
-
-var trx;
-co(function *() {
-  trx = yield cofy.fn(knex.transaction, false, knex)();
-  yield trx.raw(
+var createAdmin = function (trx) {
+  return trx.raw(
     `
     INSERT INTO users (username, password, role) VALUES
     (:username, crypt(:password, gen_salt('md5')), :role);
@@ -23,17 +20,50 @@ co(function *() {
       role: roles.ADMIN
     } 
   );
-  yield trx.into('voucher_types').insert([{
+};
+
+var createVoucherTypes = function (trx) {
+  return trx.into('voucher_types').insert([{
     name: '现金凭证'
   }, {
     name: '银行凭证'
   }]);
-}).then(function () {
-  trx.commit();
+};
+
+var createInvoiceTypes = function (trx) {
+  let rows = [
+    { 
+      name: '进项增值税', 
+      vendor_type: 'supplier',
+      purchaser_type: 'tenant',
+      is_vat: true,
+      material_type: 'inbound'
+    }, {
+      name: '销项增值税', 
+      vendor_type: 'tenant', 
+      purchaser_type: 'supplier', 
+      is_vat: true, 
+      material_type: 'outbound'
+    }, {
+      name: '普通发票', 
+      purchaser_type: 'owner', 
+      is_vat: false,
+    }
+  ];
+  return trx.batchInsert('invoice_types', rows);
+};
+
+knex.transaction(function (trx) {
+  return co(function *() {
+    yield createAdmin(trx);
+    yield createVoucherTypes(trx);
+    yield createInvoiceTypes(trx);
+  });
+})
+.then(function () {
   logger.info('completed');
   knex.destroy();
 }, function (e) {
-  trx.rollback();
   logger.error(e);
   knex.destroy();
 });
--- make-test-data.sh
+++ make-test-data.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 ./make-test-data/accounts.js | bunyan
 ./make-test-data/account-terms.js | bunyan
-./make-test-data/invoice-types.js | bunyan
 ./make-test-data/entities.js | bunyan
 ./make-test-data/material-subjects.js | bunyan
 ./make-test-data/invoices.js | bunyan
