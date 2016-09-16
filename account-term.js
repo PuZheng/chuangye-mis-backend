007f51297d0ac1287eba7da63227531124c4562d
xiechao06 <xiechoa06@gmail.com>
2016-09-17 01:15:58 +0800

    api - create account term

--- account-term.js
+++ account-term.js
@@ -1,7 +1,11 @@
+var restify = require('restify');
 var Router = require('restify-router').Router;
 var loginRequired = require('./login-required');
 var casing = require('casing');
 var knex = require('./knex');
+var logger = require('./logger');
+var R = require('ramda');
+var objDef = require('./models').account_terms;
 
 var router = new  Router();
 
@@ -17,10 +21,30 @@ var getObject = function (id) {
 router.get(
   '/list', loginRequired, 
   function (req, res, next) {
-    knex('account_terms').select('*').then(function (list) {
+    knex('account_terms')
+    .select('*')
+    .orderBy('name', 'desc')
+    .then(function (list) {
       res.json({ data: casing.camelize(list) });
       next();
     }).catch(function (e) {
+      next(e);
+    });
+  }
+);
+
+router.post(
+  '/object', loginRequired, restify.bodyParser(),
+  function (req, res, next) {
+    knex('account_terms')
+    .insert(R.pick(Object.keys(objDef), req.body))
+    .returning('id')
+    .then(function ([id]) {
+      res.json({id});
+      next();
+    })
+    .catch(function (e) {
+      logger.error(e);
       next(e);
     });
   }
