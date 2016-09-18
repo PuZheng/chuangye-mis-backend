bb38a038808daac3dad70c50b45db7304f84e797
xiechao06 <xiechoa06@gmail.com>
2016-09-18 22:04:54 +0800

    fix #38

--- voucher-subject.js
+++ voucher-subject.js
@@ -4,6 +4,9 @@ var logger = require('./logger');
 var loginRequired = require('./login-required');
 var casing = require('casing');
 var knex = require('./knex');
+var R = require('ramda');
+var co = require('co');
+var objDef = require('./models').voucher_subjects;
 
 var router = new  Router();
 
@@ -62,5 +65,36 @@ var getHints = function (req, res, next) {
 };
 
 router.get('/hints/:kw', loginRequired, getHints);
+
+var create = function (req, res, next) {
+  return co(function *() {
+    let data = R.pick(Object.keys(objDef), casing.snakeize(req.body));
+    let [{ count }] = yield knex('voucher_subjects')
+    .where('name', data.name)
+    .count();
+    if (Number(count) > 0) {
+      res.json(403, {
+        fields: {
+          name: '已经存在该名称',
+        }
+      });
+      next();
+      return;
+    }
+    let [id] = yield knex('voucher_subjects')
+    .insert(data)
+    .returning('id');
+    res.json({
+      id,
+    });
+    next();
+  })
+  .catch(function (e) {
+    logger.error(e);
+    next(e);
+  });
+};
+
+router.post('/object', loginRequired, restify.bodyParser(), create);
 
 module.exports = { router, getObject };
