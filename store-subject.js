e9d26d4d5809c95645d2f6eeb0a2ba02018f2fdc
xc <xiechao06@gmail.com>
2016-11-13 12:27:25 +0800

    api - create/update store subject

--- store-subject.js
+++ store-subject.js
@@ -4,6 +4,9 @@ var loginRequired = require('./login-required');
 var casing = require('casing');
 var knex = require('./knex');
 var restify = require('restify');
+var co = require('co');
+var R = require('ramda');
+var storeSubjectDef = require('./models').store_subjects;
 
 var getObject = function (id) {
   return knex('store_subjects').select('*')
@@ -57,6 +60,71 @@ router.get('/object/:id', loginRequired, function (req, res, next) {
   });
 });
 
+var create = function (req, res, next) {
+  return knex.transaction(function (trx) {
+    return co(function *() {
+      let { name } = req.body;
+      let [{ count }] = yield trx('store_subjects')
+      .where('name', name)
+      .count();
+      if (Number(count) > 0) {
+        res.json(400, {
+          fields: {
+            name: '该名称已经存在',
+          }
+        });
+        next();
+        return;
+      }
+      let obj = R.pick(Object.keys(storeSubjectDef),
+                       casing.snakeize(req.body));
+      yield trx('store_subjects').insert(obj).returning('id')
+      .then(function ([id]) {
+        res.json({ id });
+        next();
+      })
+      .catch(function (err) {
+        res.log.error({ err });
+        next(err);
+      });
+    });
+  });
+};
+
+router.post('/object', loginRequired, restify.bodyParser(), create);
+
+var update = function (req, res, next) {
+  return knex.transaction(function (trx) {
+    let { id } = req.params;
+    let { name, unit, acronym } = req.body;
+    return co(function *() {
+      let [{ count }] = yield trx('store_subjects').whereNot({ id })
+      .andWhere({ name })
+      .count();
+      if (Number(count) > 0) {
+        res.json(400, {
+          fields: {
+            name: '该名称已经存在',
+          }
+        });
+        next();
+        return;
+      }
+      yield trx('store_subjects').update({ name, unit, acronym })
+      .where({ id })
+      .then(function () {
+        res.json({ id });
+        next();
+      })
+      .catch(function (err) {
+        res.log.error({ err });
+        next(err);
+      });
+    });
+  });
+};
+
+router.put('/object/:id', loginRequired, restify.bodyParser(), update);
 
 module.exports = {
   getObject,
